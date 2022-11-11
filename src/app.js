b554f14a77b5a979bbd55b2cb8dd891b50942ea6
Alessandro <codpale@gmail.com>
2022-11-11 22:30:47 +0100

    Point 2: new GET /contracts endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
+const {Op} = require('sequelize')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
@@ -29,5 +30,29 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
         return
     }
     res.json(contract)
+})
+
+/**
+ * Point 2
+ *
+ * GET /contracts
+ *
+ * Returns a list of contracts belonging to a user (client or contractor): the list
+ * contains only non terminated contracts.
+ *
+ * @returns active contracts of a user profile
+ */
+ app.get('/contracts',getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    const id = req.profile.id
+    const contracts = await Contract.findAll({
+        where: {
+            [Op.and]: [
+                {[Op.or]: [{ContractorId: id}, {ClientId: id}]},
+                {status: {[Op.ne]: "terminated"}}
+            ]
+        }
+    })
+    res.json(contracts)
 })
 module.exports = app;
